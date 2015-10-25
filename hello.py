1bcc5b23935661abe9f67991c410dbec8fbbd51a
WardLu <wardlu@126.com>
2015-10-25 13:14:28 +0800

    5a

--- hello.py
+++ hello.py
@@ -1,22 +1,47 @@
+import os
 from flask import Flask, render_template,session,redirect,url_for,flash
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 from flask.ext.wtf import Form
+from flask.ext.sqlalchemy import SQLAlchemy
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 
+# basedir = os.path.abspath(os.path.dirname(__file__))
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLAlCHEMY_DATABASE_URI'] = 'mysql://root:mysql@localhost/flask'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
 
 
 class NameForm(Form):
     name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+    
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -33,9 +58,9 @@ def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
-	if old_name is not None and old_name != form.name.data:
-		flash('Looks like you have changed your name!')
-	session['name'] = form.name.data
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
         return redirect(url_for('index'))	
     return render_template('index.html', form=form, name=session.get('name'))
 
