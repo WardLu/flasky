e49d516bb0a14434872c9e0836073ec0a4aa52b9
WardLu <wardlu@126.com>
2015-10-25 14:38:36 +0800

    5c

--- hello.py
+++ hello.py
@@ -4,6 +4,7 @@ from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 from flask.ext.wtf import Form
+from flask.ext.script import Shell
 from flask.ext.sqlalchemy import SQLAlchemy
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
@@ -52,18 +53,30 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
+def make_shell_context():
+return dict(app=app, db=db, User=User, Role=Role)
+manager.add_command("shell", Shell(make_context=make_shell_context))
 
 @app.route('/', methods=['GET', 'POST'])
 def index():    
     form = NameForm()
     if form.validate_on_submit():
+        user = User.query.fliter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+        else:
+            session['know'] = True
+        session['name'] = form.name.data
+        form.name.data=''
+        return
         old_name = session.get('name')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
         session['name'] = form.name.data
         return redirect(url_for('index'))	
-    return render_template('index.html', form=form, name=session.get('name'))
-
+    return render_template('index.html', form=form, 
+        name=session.get('name'),known = session.get('konwn',False))
 
 if __name__ == '__main__':
     manager.run()
