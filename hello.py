ab2bb8e3c2d1ee58d1026bf3c3d65bb5b13deac8
WardLu <wardlu@126.com>
2015-10-25 20:37:36 +0800

    6a

--- hello.py
+++ hello.py
@@ -7,6 +7,7 @@ from flask.ext.wtf import Form
 from flask.ext.script import Shell
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.migrate import Migrate, MigrateCommand
+from flask.ext.mail import Mail,Message
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 
@@ -16,6 +17,14 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://root:mysql@localhost/flask'
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['MAIL_SERVER'] = 'stmp.126.com'
+app.config['MAIL_PORT'] = 25
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
+app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <wardlu@126.com>'
+app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
@@ -23,6 +32,7 @@ moment = Moment(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
+mail = Mail(app)
 
 
 class NameForm(Form):
@@ -61,6 +71,13 @@ def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role)
 manager.add_command("shell", Shell(make_context=make_shell_context))
 
+def send_email(to, subject, template, **kwargs):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
+        sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    mail.send(msg)
+
 @app.route('/', methods=['GET', 'POST'])
 def index():    
     form = NameForm()
@@ -69,15 +86,14 @@ def index():
         if user is None:
             user = User(username=form.name.data)
             db.session.add(user)
+            session['know']= False
+            if app.config['FLASKY_ADMIN']:
+                send_email(app.config['FLASKY_ADMIN'],'New User',
+                    'mail/new_user',user=user)
         else:
             session['know'] = True
         session['name'] = form.name.data
         form.name.data=''
-        return
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
-        session['name'] = form.name.data
         return redirect(url_for('index'))	
     return render_template('index.html', form=form, 
         name=session.get('name'),known = session.get('konwn',False))
